e5b40766e55a5b83fdc9439934cc0e2044f1c8d1
clayton-perszyk <clayperszyk@gmail.com>
2015-10-19 13:58:58 -0700

    add menu scraper to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open('http://www.allmenus.com/wa/seattle/-/?sort=popular'))
+
+doc.css('.restaurant_name a').each do |url|
+  doc = Nokogiri::HTML(open('http://www.allmenus.com/' + url.attribute('href').value))
+  address = doc.css('span[itemprop=address]')[0].content.gsub(/\n/, "").gsub(/\s+/," ")
+  restaurant_name = doc.css("h1[itemprop=name]")[0].content
+  menuItems = []
+  doc.css('.menu_item').each do |menuItem|
+    item = {}
+    item[:name] = menuItem.css('.name')[0].content
+    item[:description] = menuItem.css('.description')[0].content
+    menuItems << item
+  end
+  puts menuItems
+end
