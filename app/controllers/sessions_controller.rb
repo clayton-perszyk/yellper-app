c78f8aed7b3f20635c27c8c8272a84f1d5bb5c88
KateStearns <katestearns@mac.com>
2015-10-20 08:28:45 -0700

    views rendered for user edits

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,13 +17,26 @@ class SessionsController < ApplicationController
   end
 
   def attempt_login
+    if params[:username].present? && params[:password].present?
+      found_user = User.where(username: params[:username]).first
+      if found_user && found_user.authenticate(params[:password])
+        session[:user_id] = found_user.id
+        redirect_to user_path(User.find(found_user.id))
+      else
+        flash[:alert] = "Please enter a valid username and password"
+        redirect_to '/signup'
+      end
+    end
   end
 
   def logout
+    sessions[:user_id] = nil
+    flash[:notice] = "You are logged out"
+    redirect_to '/login'
   end
 
   private
   def user_params
-    params.require(:user).permit(:username, :password, :password_digest, :location, :image)
+    params.require(:user).permit(:username, :email, :location, :password, :password_token, :password_digest, :image)
   end
 end
