c2e3191be4570ed31ff7c5b6aad009966234ad79
Austen <rawleag@gmail.com>
2015-10-20 11:55:50 -0700

    nav bar search

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,15 @@
 class FoodsController < ApplicationController
+
   def index
-    @food = Food.fuzzy_search('taco')
-  end
+   client = Yelp::Client.new({ consumer_key: 'xlKUjkdAZRFouGJCpfAbyw',
+                             consumer_secret: 'wzC3w_0pyw9yu3lPUH9goZoJw-E',
+                             token: 'Z7FZV4BF6Ccxksh1I4TIQ_NPVAvUj727',
+                             token_secret: 'iqLpS5jCKQRBrIb3b-aQcptma2s'
+                           })
+
+   @restaurantName = Food.all.where(name:'Green Bean Wok')[0].restaurant.name
+
+   @food = client.business('Phayathai-Cuisine-seattle')
+
+ end
 end
