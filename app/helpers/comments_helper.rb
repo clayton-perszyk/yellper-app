62039210a5832218e40af4c01af56bcb7a345f1e
clayton-perszyk <clayperszyk@gmail.com>
2015-10-22 13:52:12 -0700

    Style restaurant show view

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,9 +1,10 @@
 module CommentsHelper
   def num_to_stars(number)
-    starred_rating = ""
+    starred_rating = "<ul class='stars'>"
     empty_stars = 5 - number
-    number.times { |n| starred_rating << '★' }
-    empty_stars.times { |n| starred_rating << '☆' }
-    starred_rating
+    number.times { |n| starred_rating << '<li>★</li>' }
+    empty_stars.times { |n| starred_rating << '<li>☆</li>' }
+    starred_rating << "</ul>"
+    starred_rating.html_safe
   end
 end
