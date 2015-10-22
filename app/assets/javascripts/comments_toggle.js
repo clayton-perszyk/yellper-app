ed83421737c25b0969b330cdd95e056148274d9f
Austen <rawleag@gmail.com>
2015-10-22 08:52:54 -0700

    jQuery on comments working

--- app/assets/javascripts/comments_toggle.js
+++ app/assets/javascripts/comments_toggle.js
@@ -13,16 +13,16 @@ $(document).ready(function(){
 
   $('.edit-comment').on('click', function(e) {
     e.preventDefault();
-    var self = this
-    $(this).parent().children('.edit').toggle('fast', function () {
-      var display = $(self).css('display');
+    var $self = $(this)
+    $(this).parent().find('.edit').toggle('fast', function () {
+      var display = $self.css('display');
       console.log(this)
-      if ($(self).text() == 'Cancel') {
-        $('.original-comment').show();
-        $(self).text("Edit Comment");
+      if ($self.text() == 'Cancel') {
+        $self.find('.original-comment').show();
+        $self.text("Edit Comment");
       } else {
-        $('.original-comment').hide();
-        $(self).text("Cancel");
+        $self.find('.original-comment').hide();
+        $self.text("Cancel");
       }
     });
   });
