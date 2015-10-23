e444758cc6bd4dd95351a8748718a07dbfb382f4
clayton-perszyk <clayperszyk@gmail.com>
2015-10-23 15:34:31 -0700

    Add serve_static_assests to config attempt 5

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module Yellper
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += [
+      
+    ]
 
     config.serve_static_files = true
 
