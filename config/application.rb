ad743c0c720f924e7784935e968e3b351f1c1d35
clayton-perszyk <clayperszyk@gmail.com>
2015-10-23 16:03:55 -0700

    Add serve_static_assests to config attempt 10

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Yellper
     # config.i18n.default_locale = :de
 
     config.serve_static_files = true
-
+    
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
